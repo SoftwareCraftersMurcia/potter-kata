927acfa8ab46a556718483169e74bee0509eb8c2
Juan Sánchez <jsmontesinos@gmail.com>
2023-04-21 19:28:50 +0200

    Add a case with two books differnte bt other is the same

--- kotlin/src/test/kotlin/MainKtTest.kt
+++ kotlin/src/test/kotlin/MainKtTest.kt
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test
 class MainKtTest {
     @Test
     fun empty_bag() {
-        Assertions.assertEquals(0, price(listOf<Int>()))
+        Assertions.assertEquals(0.0, price(listOf<Int>()))
     }
 
     @Test
     fun one_item_in_bag_has_no_discount() {
-        Assertions.assertEquals(8, price(listOf<Int>(1)))
+        Assertions.assertEquals(8.0, price(listOf<Int>(1)))
     }
 
     @Test
@@ -19,7 +19,7 @@ class MainKtTest {
 
     @Test
     fun two_same_books_in_bag_has_no_discount() {
-        Assertions.assertEquals(8 * 2, price(listOf<Int>(1, 1)))
+        Assertions.assertEquals((8 * 2).toDouble(), price(listOf<Int>(1, 1)))
     }
 
     @Test
@@ -37,13 +37,21 @@ class MainKtTest {
         Assertions.assertEquals(8 * 5 * 0.75, price(listOf<Int>(1, 2, 3, 4, 5)))
     }
 
-    private fun price(books: List<Int>): Any {
-        return when (books.toSet().size) {
-            2 ->  books.size * 8 * 0.95
-            3 ->  books.size * 8 * 0.90
-            4 ->  books.size * 8 * 0.80
-            5 ->  books.size * 8 * 0.75
-            else ->  books.size * 8
-        }
+    @Test
+    fun bag_with_theee_books_two_or_theme_are_equals_has_5_percent_of_discount_over_two_of_theme() {
+        Assertions.assertEquals(8 * 2 * 0.95 + 8, price(listOf<Int>(1, 1, 2)))
+    }
+
+    private fun price(books: List<Int>): Double {
+        val diffentBooks = books.toSet().size;
+        val differentBooksPrice =  when (diffentBooks) {
+            2 ->  diffentBooks * 8 * 0.95
+            3 ->  diffentBooks * 8 * 0.90
+            4 ->  diffentBooks * 8 * 0.80
+            5 ->  diffentBooks * 8 * 0.75
+            else ->  diffentBooks * 8
+        }.toDouble()
+
+        return differentBooksPrice + ((books.size - diffentBooks) * 8); 
     }
 }
