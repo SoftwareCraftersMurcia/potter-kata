8fd68d122f3af3af6158e0675a51f2c157b389cd
Juan Sánchez <jsmontesinos@gmail.com>
2023-04-21 18:51:21 +0200

    Add new test for same books (WIP)

--- kotlin/src/test/kotlin/MainKtTest.kt
+++ kotlin/src/test/kotlin/MainKtTest.kt
@@ -17,10 +17,16 @@ class MainKtTest {
         Assertions.assertEquals(8 * 2 * 0.95, price(listOf<Int>(1, 2)))
     }
 
+    @Test
+    fun two_same_items_in_bag_has_no_discount() {
+        Assertions.assertEquals(8 * 2, price(listOf<Int>(1, 1)))
+    }
+
     private fun price(books: List<Int>): Any {
-        if (books.size == 2) {
+        if (setOf(books).size == 2) {
             return books.size * 8 * 0.95
         }
+
         return books.size * 8
     }
 }
