c19e862b2be9fc074c669626e387737195a2c70f
F0rno <pforper2412@g.educaand.es>
2023-04-21 18:28:51 +0200

    test empty_bag

---
+++ kotlin/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+    @Test
+    fun empty_bag() {
+        Assertions.assertEquals(0, price(listOf<Int>()))
+    }
+
+    private fun price(books: List<Int>): Int {
+        return 0
+    }
+}
