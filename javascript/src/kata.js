c669fdf67fb8f0b7b717eb1d936781b3e52a5951
Pascual Cano <pascual@allhuman.com>
2023-04-21 19:59:38 +0200

    Case of [1,1,2,2]

--- javascript/src/kata.js
+++ javascript/src/kata.js
@@ -1,25 +1,22 @@
 const DISCOUNTS = {
-    0: 0,
-    1: 0,
-    2: 5,
-    3: 10,
-    4: 20,
-    5: 25
-}
-
-const calculatePrice = (basket = []) => {
-    const basketSize = basket.length;
-    const sizeOfSets = (new Set(basket)).size;
-    const numberOfSets = sizeOfSets > 0 ?  Math.floor(basketSize / sizeOfSets) : 0;    
-    const discount = DISCOUNTS[sizeOfSets] / 100;
-    console.log(sizeOfSets,numberOfSets,discount)
-    const fullPrice = 8;
-    const discountedPrice = 8 * (1 - discount);
-    const fullPriceBooks = basketSize - sizeOfSets;
-    const discountedPriceBooks = sizeOfSets * numberOfSets;    
-    return fullPriceBooks * fullPrice + discountedPriceBooks * discountedPrice;
+  0: 0,
+  1: 0,
+  2: 5,
+  3: 10,
+  4: 20,
+  5: 25,
 };
 
+const calculatePrice = (basket = []) => {
+  const basketSize = basket.length;
+  const sizeOfSets = new Set(basket).size;
+  const numberOfSets = sizeOfSets > 0 ? Math.floor(basketSize / sizeOfSets) : 0;
+  const discount = DISCOUNTS[sizeOfSets] / 100;
+  const fullPrice = 8;
+  const discountedPrice = 8 * (1 - discount);
+  const discountedPriceBooks = sizeOfSets * numberOfSets;
+  const fullPriceBooks = basketSize - discountedPriceBooks;
+  return fullPriceBooks * fullPrice + discountedPriceBooks * discountedPrice;
+};
 
-
-module.exports = {calculatePrice};
+module.exports = { calculatePrice };
--- javascript/test/kata.test.js
+++ javascript/test/kata.test.js
@@ -1,54 +1,53 @@
-const { calculatePrice } = require('../src/kata');
+const { calculatePrice } = require("../src/kata");
 
 describe("Kata", function () {
   it("returns 0 for an empty basket", function () {
-      const result = calculatePrice([]);
-      expect(result).toBe(0);
+    const result = calculatePrice([]);
+    expect(result).toBe(0);
   });
 
-  it("returns 8 for just one book", function(){
-      const result = calculatePrice([1]);
-      expect(result).toBe(8);
+  it("returns 8 for just one book", function () {
+    const result = calculatePrice([1]);
+    expect(result).toBe(8);
   });
 
-  it("returns 8 for *any* book", function(){
-      const result = calculatePrice([2]);
-      expect(result).toBe(8);
+  it("returns 8 for *any* book", function () {
+    const result = calculatePrice([2]);
+    expect(result).toBe(8);
   });
 
-  it.only("returns 16 for two books of the same volumen", function(){
-      const result = calculatePrice([1,1]);
-      expect(result).toBe(16);
+  it("returns 16 for two books of the same volumen", function () {
+    const result = calculatePrice([1, 1]);
+    expect(result).toBe(16);
   });
-  
-  it("returns 15.20 for two different books", function(){
-      const result = calculatePrice([1,2]);
-      expect(result).toBe(15.2);
-  });  
-  
-  it("returns 21.6 for three different books", function(){
-      const result = calculatePrice([1,2,3]);
-      expect(result).toBe(21.6);
-  });  
 
-  it("returns 25.6 for four different books", function(){
-      const result = calculatePrice([1,2,3,4]);
-      expect(result).toBe(25.6);
-  });  
-  
-  it("returns 30 for five different books", function(){
-      const result = calculatePrice([1,2,3,4,5]);
-      expect(result).toBe(30);
-  });   
-  
-  it("returns 23.20 for two 1s and a 2", function(){
-      const result = calculatePrice([1,1,2]);
-      expect(result).toBe(23.2);
-  });   
-  
-  it("returns 30.40 for two 1s and two 2s", function(){
-      const result = calculatePrice([1,1,2,2]);
-      expect(result).toBe(30.40);
-  });   
+  it("returns 15.20 for two different books", function () {
+    const result = calculatePrice([1, 2]);
+    expect(result).toBe(15.2);
+  });
 
+  it("returns 21.6 for three different books", function () {
+    const result = calculatePrice([1, 2, 3]);
+    expect(result).toBe(21.6);
+  });
+
+  it("returns 25.6 for four different books", function () {
+    const result = calculatePrice([1, 2, 3, 4]);
+    expect(result).toBe(25.6);
+  });
+
+  it("returns 30 for five different books", function () {
+    const result = calculatePrice([1, 2, 3, 4, 5]);
+    expect(result).toBe(30);
+  });
+
+  it("returns 23.20 for two 1s and a 2", function () {
+    const result = calculatePrice([1, 1, 2]);
+    expect(result).toBe(23.2);
+  });
+
+  it("returns 30.40 for two 1s and two 2s", function () {
+    const result = calculatePrice([1, 1, 2, 2]);
+    expect(result).toBe(30.4);
+  });
 });
